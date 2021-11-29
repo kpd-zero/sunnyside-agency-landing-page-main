46cf5d4a542c0b281fb84af75aec2b53d7256433
kpd-zero <56587947+kpd-zero@users.noreply.github.com>
2021-11-30 01:58:27 +0800

    Update main.js

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ window.onload = function digital_fn() {
 
     toggle.addEventListener('click', function(event){
         collapse.classList.toggle('active')
-        console.log(toggle)
     });
 
 }
