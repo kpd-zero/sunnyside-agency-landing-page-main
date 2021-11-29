34ca93be40f43617d9f17b72a5fae66d75c60bd2
kpd-zero <56587947+kpd-zero@users.noreply.github.com>
2021-11-30 01:32:14 +0800

    aaaaa
    aaaaaa

---
+++ main.js
@@ -0,0 +1,12 @@
+//execute script after page load
+window.onload = function digital_fn() {
+    //toggle button
+    let toggle = document.querySelector(".toggle-btn");
+    let collapse = document.querySelector(".collapse");
+
+    toggle.addEventListener('click', function(event){
+        collapse.classList.toggle('active')
+        console.log(toggle)
+    });
+
+}
